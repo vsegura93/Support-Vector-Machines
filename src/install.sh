f17ed31567844c14d0bd8b49ec24d221d89317cd
vsegura93 <victor.seguratir@e-campus.uab.cat>
2017-04-11 17:58:01 +0200

    Update file src/install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Install enviroment
+# Author: Victor Segura Tirado
+# mail: victor.seguratir@e-campus.uab.cat
+
+# Delete files form last installation
+rm lib*
+rm CMakeCache.txt
+rm -r CMakeFiles
+rm cmake_install.cmake
+
+# Make again
 cmake -DCMAKE_INSTALL_PREFIX="." ..
