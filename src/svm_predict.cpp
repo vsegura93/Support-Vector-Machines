dce54be31cc0d59f8037dc1e573a7c583de6b252
vsegura93 <victor.seguratir@e-campus.uab.cat>
2017-05-22 20:52:17 +0200

    Included arguments on svm_predict binary

--- src/svm_predict.cpp
+++ src/svm_predict.cpp
@@ -59,62 +59,67 @@ vector<vector<float>> generateData(int problemSize, int featureNum)
 ************** MAIN ***************
 **********************************/
 
-int main()
+int main(int argc, char **argv)
 {
-	// Load model
-	cout << "Loading model..." << endl;
-	svm_model *model = svm_load_model("model");
-	cout << "Loading model: DONE" << endl;
-	struct svm_node *x_space;
+	if(argc < 2)
+	{cout << "Missing image argument. Usage: svm_predict <image directory>" << endl;
+	}else{
 
-	// Load image
-	string dir = string("../dataset/4/image_0037.jpg");
-	Mat image = imread(dir, 0);	
-	cout << "Image: " << dir <<  endl;
+		// Load model
+		cout << "Loading model..." << endl;
+		svm_model *model = svm_load_model("model");
+		cout << "Loading model: DONE" << endl;
+		struct svm_node *x_space;
 
-	// Extract features
-	computeHOGDescriptors(image);
+		// Load image
+		string dir = string(argv[1]);
+		Mat image = imread(dir, 0);	
+		cout << "Image: " << dir <<  endl;
 
-	int sizeOfProblem = 1;				// number of lines with labels
-	int elements = descriptorsValues.size();	// number of features for each data vector
-	vector<vector<float>> data = generateData(sizeOfProblem, elements);
+		// Extract features
+		computeHOGDescriptors(image);
 
-	// initialize the size of the problem with just an int	
-	prob.l = sizeOfProblem;
+		int sizeOfProblem = 1;				// number of lines with labels
+		int elements = descriptorsValues.size();	// number of features for each data vector
+		vector<vector<float>> data = generateData(sizeOfProblem, elements);
+
+		// initialize the size of the problem with just an int	
+		prob.l = sizeOfProblem;
 	
-	// here we need to give some memory to our structures
-	// @param prob.l = number of labels
-	// @param elements = number of features for each label
-	prob.y = Malloc(double, prob.l); 				// space for prob.l doubles
-	prob.x = Malloc(struct svm_node *, prob.l); 			// space for prob.l pointers to struct svm_node
-	x_space = Malloc(struct svm_node, (elements+1) * prob.l); 	// memory for pairs of index/value
+		// here we need to give some memory to our structures
+		// @param prob.l = number of labels
+		// @param elements = number of features for each label
+		prob.y = Malloc(double, prob.l); 				// space for prob.l doubles
+		prob.x = Malloc(struct svm_node *, prob.l); 			// space for prob.l pointers to struct svm_node
+		x_space = Malloc(struct svm_node, (elements+1) * prob.l); 	// memory for pairs of index/value
 
-	// initialize the svm_node vector with input data array as follows:
-	int j=0; 					// counter to traverse x_space[i];
-	for (int i=0;i < prob.l; i++)
-	{
-		// set i-th element of prob.x to the address of x_space[j]. 
-		// elements from x_space[j] to x_space[j+data[i].size] get filled right after next line
-		prob.x[i] = &x_space[j];
-		for (int k=0; k<data[i].size(); ++k, ++j)
+		// initialize the svm_node vector with input data array as follows:
+		int j=0; 					// counter to traverse x_space[i];
+		for (int i=0;i < prob.l; i++)
 		{
-			x_space[j].index=k+1; 		// index of value
-			x_space[j].value=data[i][k]; 	// value
+			// set i-th element of prob.x to the address of x_space[j]. 
+			// elements from x_space[j] to x_space[j+data[i].size] get filled right after next line
+			prob.x[i] = &x_space[j];
+			for (int k=0; k<data[i].size(); ++k, ++j)
+			{
+				x_space[j].index=k+1; 		// index of value
+				x_space[j].value=data[i][k]; 	// value
+				// cout<<"x_space["<<j<<"].index = "<<x_space[j].index<<endl;
+				// cout<<"x_space["<<j<<"].value = "<<x_space[j].value<<endl;
+			}
+			x_space[j].index=-1;			// state the end of data vector
+			x_space[j].value=0;
 			// cout<<"x_space["<<j<<"].index = "<<x_space[j].index<<endl;
-			// cout<<"x_space["<<j<<"].value = "<<x_space[j].value<<endl;
+			// cout<<"x_space["<<j<<"].value = "<<x_space[j].value<<endl;		
+			j++;
 		}
-		x_space[j].index=-1;			// state the end of data vector
-		x_space[j].value=0;
-		// cout<<"x_space["<<j<<"].index = "<<x_space[j].index<<endl;
-		// cout<<"x_space["<<j<<"].value = "<<x_space[j].value<<endl;		
-		j++;
-	}
 
-	// double svm_predict(const struct svm_model *model, const struct svm_node *x);
-	cout << "Making predictions..." << endl;
-	double retval = svm_predict(model, x_space);
-	cout << "Making predictions: DONE" << endl;
-	printf("PROBABILITY CLASS: %f\n",retval);
+		// double svm_predict(const struct svm_model *model, const struct svm_node *x);
+		cout << "Making predictions..." << endl;
+		double retval = svm_predict(model, x_space);
+		cout << "Making predictions: DONE" << endl;
+		printf("PROBABILITY CLASS: %f\n",retval);
+	}
 
 	return 0;
 }
